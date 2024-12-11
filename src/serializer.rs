2d27c306f3c36a171561405e4af321159b748873
Anish Poudel <anishpoudel@Anishs-MacBook-Air.local>
2024-12-11 13:57:16 +0545

    code refactor

--- src/serializer.rs
+++ src/serializer.rs
@@ -4,22 +4,16 @@ use crate::models::_entities::{task, user, user_profile};
 
 #[derive(Debug, Serialize)]
 pub struct UserSerializer {
+    pub id: i32,
     pub name: String,
     pub username: String,
     pub email: String,
-}
-
-#[derive(Debug, Serialize)]
-pub struct UserWithProfileSerializer {
-    pub name: String,
-    pub username: String,
-    pub email: String,
-    pub profile: Option<user_profile::Model>,
 }
 
 impl From<user::Model> for UserSerializer {
     fn from(value: user::Model) -> Self {
         Self {
+            id: value.id,
             name: value.name,
             username: value.username,
             email: value.email,
@@ -27,15 +21,44 @@ impl From<user::Model> for UserSerializer {
     }
 }
 
+#[derive(Debug, Serialize)]
+pub struct UserProfileSerializer {
+    pub id: i32,
+    pub address: Option<String>,
+    pub mobile_number: Option<String>,
+}
+
+impl From<user_profile::Model> for UserProfileSerializer {
+    fn from(value: user_profile::Model) -> Self {
+        Self {
+            id: value.id,
+            address: value.address,
+            mobile_number: value.mobile_number,
+        }
+    }
+}
+
+#[derive(Debug, Serialize)]
+pub struct UserWithProfileSerializer {
+    pub id: i32,
+    pub name: String,
+    pub username: String,
+    pub email: String,
+    pub profile: Option<UserProfileSerializer>,
+}
+
 impl From<(user::Model, Option<user_profile::Model>)> for UserWithProfileSerializer {
     fn from(value: (user::Model, Option<user_profile::Model>)) -> Self {
         let (user, profile) = value;
 
+        let profile_serializer = profile.map(UserProfileSerializer::from);
+
         Self {
+            id: user.id,
             name: user.name,
             username: user.username,
             email: user.email,
-            profile,
+            profile: profile_serializer,
         }
     }
 }
@@ -59,34 +82,6 @@ impl From<task::Model> for TaskSerializer {
             status: value.status,
             date_created: value.date_created,
             date_updated: value.date_updated,
-        }
-    }
-}
-
-impl UserWithProfileSerializer {
-    pub fn new(
-        name: String,
-        username: String,
-        email: String,
-        profile: Option<user_profile::Model>,
-    ) -> Self {
-        Self {
-            name,
-            username,
-            email,
-            profile,
-        }
-    }
-
-    pub fn from_user_and_profile_model(
-        user: user::Model,
-        user_profile: Option<user_profile::Model>,
-    ) -> Self {
-        Self {
-            name: user.name,
-            username: user.username,
-            email: user.email,
-            profile: user_profile,
         }
     }
 }
